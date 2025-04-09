85588a3194fa495fcbe06267b4fc1930834354c1
AlbertoCancela <albertocancelaa@gmail.com>
2025-04-09 16:57:36 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram</title>
+    <link rel="stylesheet" href="src/css/main.css">
+    <link rel="icon" href="assets/images/Instagram_icon.png.webp" type="">
+</head>
+<body>
+    <main id="main-wrapper">
+        <div class="wrapper">
+            <div class="main-image">
+                <img src="assets/images/instagram-loginImages.png" alt="">
+            </div>
+            <div class="main-form">
+                <div class="form">
+                    <div class="form-title">
+                        <img src="assets/images/instagram-titleForm.png" alt="">
+                    </div>
+                    <div class="form-form login-container">
+                        <input type="text" placeholder="Teléfono, usuario o correo electrónico" class="login-input">
+                        <input type="password" placeholder="Contraseña" class="login-input">
+                        <button class="login-button">Entrar</button>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
