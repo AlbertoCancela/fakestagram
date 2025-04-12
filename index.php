c5ff0650268d62a093c6060e73e08031851f3d58
AlbertoCancela <albertocancelaa@gmail.com>
2025-04-12 12:17:38 -0600

    hey

--- index.php
+++ index.php
@@ -22,11 +22,46 @@
                         <input type="text" placeholder="Teléfono, usuario o correo electrónico" class="login-input">
                         <input type="password" placeholder="Contraseña" class="login-input">
                         <button class="login-button">Entrar</button>
+                        <img src="assets/images/o.png" alt="">
+                        <p id="login-startWFB">
+                            <svg id="login-start-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                            </svg>   
+                            <b>Iniciar sesión con facebook</b>
+                        </p>
+                        <a id="login-forgotPswd" href="www.google.com">¿Has olvidado la contraseña?</a>
+
+                        <p id="login-dontHA">¿No tienes una cuenta? <a id="login-register">Regístrate</a></p>
+                        <a id="login-downloadApp">Descarga la aplicación</a>
+                        <div class="login-appStores">
+                            <img onclick="window.location.href='https:www.google.com';" src="assets/images/appstore.png" alt="">
+                            <img src="assets/images/playstore.png" alt="">
+                        </div>
                     </div>
-                    <div></div>
                 </div>
             </div>
         </div>
     </main>
+    <div class="main-footer">
+                        <div class="footer-links">
+                            <span>Meta</span>
+                            <span>Información</span>
+                            <span>Blog</span>
+                            <span>Empleo</span>
+                            <span>Ayuda</span>
+                            <span>API</span>
+                            <span>Privacidad</span>
+                            <span>Condiciones</span>
+                            <span>Ubicaciones</span>
+                            <span>Instagram Lite</span>
+                            <span>Threads</span>
+                            <span>Subir contactos y personas no usuarias</span>
+                            <span>Meta Verified</span>
+                        </div>
+                        <div class="footer-bottom">
+                            <span>Español (España) ▼</span>
+                            <span>© 2025 Instagram from Meta</span>
+                        </div>
+                    </div>
 </body>
 </html>
